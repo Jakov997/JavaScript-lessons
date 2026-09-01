82006321475e84ba160ae34aa1213014810ba02e
Jakov Polkin <jahanger1119@gmail.com>
2026-09-01 22:41:20 +0700

    Создал ветку lesson1

---
+++ script.js
@@ -0,0 +1,11 @@
+let title;
+title = "Hello, World!";
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+fullPrice = 3500;
+let adaptive;
+
+alert('Приветствую Вас на сайте');
+console.log('Сообщение в консоль, для любопытных посетителей')
