180e6cd20bdc64039cab1068362f327896f12062
Jakov Polkin <jahanger1119@gmail.com>
2026-09-03 23:35:35 +0700

    Выполнил задание урока 2

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let title;
-title = "Hello, World!";
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-fullPrice = 3500;
-let adaptive;
+title = "My project about JavaScript";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 1500;
+let rollback = 7;
+let fullPrice = 50000;
+let adaptive = true;
 
-alert('Приветствую Вас на сайте');
-console.log('Сообщение в консоль, для любопытных посетителей')
+console.log(title + "\n" + fullPrice + "\n" + adaptive);
+console.log(screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+ let lowCase = screens.toLowerCase().split(', ');
+console.log(lowCase);
+let percent = (fullPrice*(rollback/100));
+console.log(percent);
