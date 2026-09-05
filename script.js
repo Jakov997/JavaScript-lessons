1a53091d86d6480f2fcc8d06d203adcdb3af4bee
Jakov Polkin <jahanger1119@gmail.com>
2026-09-05 23:10:10 +0700

    Сделал не большие правки привел код согласно глобального стиля

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 let title = prompt("Введите название проекта");
 let screens = prompt("Введите типы экранов через запятую");
 let screenPrice = Number(prompt("Введите стоимость данной работы"));
-let rollback = 7;
 let adaptive = confirm("Будет ли адаптивная верстка? ");
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = Number(prompt("Введите стоимость дополнительной услуги 1"));
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice2 = Number(prompt("Введите стоимость дополнительной услуги 2"));
+let rollback = 7;
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let percent = fullPrice * (rollback / 100);
+let servicePercentPrice = fullPrice - percent;
+let lowCase = screens.toLowerCase().split(", ");
+
+
 if (adaptive) {
   console.log("Адаптивная верстка необходима");
 } else {
   console.log("Адаптивная верстка не нужна");
 }
-
-let service1 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice1 = Number(prompt("Введите стоимость дополнительной услуги 1"));
-let service2 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice2 = Number(prompt("Введите стоимость дополнительной услуги 2"));
-
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-
-let percent = fullPrice * (rollback / 100);
-let servicePercentPrice = fullPrice - percent;
-console.log(`Стоимость с учетом отката: ${Math.ceil(servicePercentPrice)}`);
 
 if (fullPrice > 30000) {
     console.log("Даем скидку в 10%");
@@ -31,9 +30,11 @@ if (fullPrice > 30000) {
 }
 
 
+console.log(`Стоимость с учетом отката: ${Math.ceil(servicePercentPrice)}`);
 console.log(title + "\n" + fullPrice + "\n" + adaptive);
 console.log(screens.split(", ").length);
 console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
-let lowCase = screens.toLowerCase().split(", ");
+console.log(`Полная стоимость ${fullPrice} рублей`);
+
 console.log(lowCase);
 
