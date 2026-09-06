4b38e695992ead995781b3f5cc7cb7e95bb16a19
Jakov Polkin <jahanger1119@gmail.com>
2026-09-06 12:04:23 +0700

    Запушил 6-й урок

--- script.js
+++ script.js
@@ -1,102 +1,62 @@
-let title = prompt("Введите название проекта");
-let screens = prompt("Введите типы экранов через запятую");
-let screenPrice;
-let screenPriceInput;
-let adaptive = confirm("Будет ли адаптивная верстка? ");
-let service1 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice1 = prompt("Введите стоимость дополнительной услуги 1");
-let service2 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice2 = prompt("Введите стоимость дополнительной услуги 2");
-let rollback = 7;
+function createGame() {
+  const secretNumber = Math.floor(Math.random() * 100) + 1;
 
-let lowCase = screens.toLowerCase().split(", ");
+  function guessNumber() {
+    const userAnswer = prompt("Угадай число от 1 до 100");
 
-if (adaptive) {
-  console.log("Адаптивная верстка необходима");
+    if (userAnswer === null ) {
+      alert("Игра окончена");
+      return;
+    }
+
+    if (userAnswer.trim() === "") {
+      alert("Введите число!!!");
+      guessNumber();
+      return;
+    }
+
+    if (Number(userAnswer) <= 0) {
+      alert("Введите число больше 0");
+      guessNumber();
+      return;
+    }
+
+    if (userAnswer > 100) {
+      alert("Введите число меньшее или равное 100");
+      guessNumber();
+      return;
+    }
+
+    if (Number.isNaN(userAnswer)) {
+      alert("Введи число!");
+      guessNumber();
+      return;
+    }
+
+    const number = Number(userAnswer);
+
+    if (number > secretNumber) {
+      alert("Загаданное число меньше");
+      guessNumber();
+      return;
+    }
+
+    if (number < secretNumber) {
+      alert("Загаданное число больше");
+      guessNumber();
+      return;
+    }
+
+    alert("Поздравляю, Вы угадали!!!");
+  }
+
+  guessNumber();
+}
+
+const startGame = confirm("Начать игру?");
+
+if (startGame) {
+  createGame();
 } else {
-  console.log("Адаптивная верстка не нужна");
+  alert("Игра окончена");
 }
-
-do {
-  screenPriceInput = prompt("Введите стоимость данной работы");
-  screenPrice = Number(screenPriceInput);
-} while (
-  screenPriceInput === null ||
-  screenPriceInput.trim() === "" ||
-  !Number.isFinite(screenPrice)
-);
-
-let getAllServicePrices = function (a, b) {
-  let price1 = Number(a);
-  let price2 = Number(b);
-
-  if (
-    a === null ||
-    b === null ||
-    a.trim() === "" ||
-    b.trim() === "" ||
-    !Number.isFinite(price1) ||
-    !Number.isFinite(price2)
-  ) {
-    return 0;
-  }
-
-  return price1 + price2;
-};
-let allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
-
-function getFullPrice() {
-  return screenPrice + allServicePrices;
-}
-
-let fullPrice = getFullPrice();
-
-let percent = fullPrice * (rollback / 100);
-
-let discount = function (price) {
-  if (price > 30000) {
-    return "Даем скидку в 10%";
-  } else if (price > 15000 && price <= 30000) {
-    return "Даем скидку в 5%";
-  } else if (price > 0 && price <= 15000) {
-    return "Скидка не предусмотрена";
-  } else if (price <= 0) {
-    return "Что то пошло не так";
-  }
-};
-
-function getTitle(title) {
-  let clearTitle = title.trim();
-  if (clearTitle === "") {
-    return "";
-  }
-  return clearTitle[0].toUpperCase() + clearTitle.slice(1).toLowerCase();
-}
-
-title = getTitle(title);
-
-function getServicePercentPrices() {
-  return fullPrice - percent;
-}
-
-let servicePercentPrices = getServicePercentPrices();
-
-const counterPotatoes = function (potatoes) {
-  for (let i = 0; i < potatoes.length; i++) {
-    console.log(`Картошка №${i + 1} почищена!`);
-  }
-};
-
-counterPotatoes([1, 2, 3, 4, 5]);
-
-console.log(`Название проекта: ${title}`);
-console.log(discount(fullPrice));
-console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
-console.log(typeof allServicePrices + " -Это тип переменной allServicePrices");
-console.log(`Сумма всех дополнительных услуг: ${allServicePrices} рублей`);
-console.log(
-  `Стоимость верстки и всех дополнительных услуг: ${fullPrice} рублей`,
-);
-console.log(`Стоимость с учетом отката: ${Math.ceil(servicePercentPrices)}`);
-
-console.log(lowCase);
