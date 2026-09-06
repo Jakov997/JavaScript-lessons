199905c50dbcb3c33156e14516a9929a241667bd
Jakov Polkin <jahanger1119@gmail.com>
2026-09-06 15:05:13 +0700

    Запушил урок 7

--- script.js
+++ script.js
@@ -1,102 +1,126 @@
-let title = prompt("Введите название проекта");
-let screens = prompt("Введите типы экранов через запятую");
-let screenPrice;
-let screenPriceInput;
-let adaptive = confirm("Будет ли адаптивная верстка? ");
-let service1 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice1 = prompt("Введите стоимость дополнительной услуги 1");
-let service2 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice2 = prompt("Введите стоимость дополнительной услуги 2");
-let rollback = 7;
+const appData = {
+  title: "",
+  screens: "",
+  screenPrice: 0,
+  screenPriceInput: 0,
+  adaptive: true,
+  service1: "",
+  servicePrice1: 0,
+  service2: "",
+  servicePrice2: 0,
+  rollback: 7,
 
-let lowCase = screens.toLowerCase().split(", ");
+  asking() {
+    appData.title =
+      prompt("Введите название проекта", "Рекламный лендинг") ?? "";
+    appData.screens =
+      prompt(
+        "Введите типы экранов через запятую",
+        "Десктоп, Мобильный и тд",
+      ) ?? "";
 
-if (adaptive) {
-  console.log("Адаптивная верстка необходима");
-} else {
-  console.log("Адаптивная верстка не нужна");
-}
+    do {
+      appData.screenPriceInput = prompt("Введите стоимость данной работы");
+      appData.screenPrice = Number(appData.screenPriceInput);
+    } while (
+      appData.screenPriceInput === null ||
+      appData.screenPriceInput.trim() === "" ||
+      !Number.isFinite(appData.screenPrice)
+    );
 
-do {
-  screenPriceInput = prompt("Введите стоимость данной работы");
-  screenPrice = Number(screenPriceInput);
-} while (
-  screenPriceInput === null ||
-  screenPriceInput.trim() === "" ||
-  !Number.isFinite(screenPrice)
-);
+    appData.adaptive = confirm("Будет ли адаптивная верстка?");
+    appData.service1 = prompt("Какой дополнительный тип услуги нужен?");
+    appData.servicePrice1 = prompt("Введите стоимость дополнительной услуги 1");
+    appData.service2 = prompt("Какой дополнительный тип услуги нужен?");
+    appData.servicePrice2 = prompt("Введите стоимость дополнительной услуги 2");
+  },
 
-let getAllServicePrices = function (a, b) {
-  let price1 = Number(a);
-  let price2 = Number(b);
+  getAllServicePrices() {
+    const price1 = Number(appData.servicePrice1);
+    const price2 = Number(appData.servicePrice2);
 
-  if (
-    a === null ||
-    b === null ||
-    a.trim() === "" ||
-    b.trim() === "" ||
-    !Number.isFinite(price1) ||
-    !Number.isFinite(price2)
-  ) {
-    return 0;
-  }
+    if (
+      appData.servicePrice1 === null ||
+      appData.servicePrice2 === null ||
+      appData.servicePrice1.trim() === "" ||
+      appData.servicePrice2.trim() === "" ||
+      !Number.isFinite(price1) ||
+      !Number.isFinite(price2)
+    ) {
+      return 0;
+    }
 
-  return price1 + price2;
-};
-let allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+    return price1 + price2;
+  },
 
-function getFullPrice() {
-  return screenPrice + allServicePrices;
-}
+  getFullPrice() {
+    return appData.screenPrice + appData.getAllServicePrices();
+  },
 
-let fullPrice = getFullPrice();
+  getTitle() {
+    const clearTitle = this.title.trim();
 
-let percent = fullPrice * (rollback / 100);
+    if (clearTitle === "") {
+      return "";
+    }
 
-let discount = function (price) {
-  if (price > 30000) {
-    return "Даем скидку в 10%";
-  } else if (price > 15000 && price <= 30000) {
-    return "Даем скидку в 5%";
-  } else if (price > 0 && price <= 15000) {
-    return "Скидка не предусмотрена";
-  } else if (price <= 0) {
+    return clearTitle[0].toUpperCase() + clearTitle.slice(1).toLowerCase();
+  },
+
+  getServicePercentPrices() {
+    const percent = appData.getFullPrice() * (appData.rollback / 100);
+    return Math.ceil(appData.getFullPrice() - percent);
+  },
+
+  discount(price) {
+    if (price > 30000) {
+      return "Даем скидку в 10%";
+    }
+
+    if (price > 15000) {
+      return "Даем скидку в 5%";
+    }
+
+    if (price > 0) {
+      return "Скидка не предусмотрена";
+    }
+
     return "Что то пошло не так";
-  }
+  },
+
+  logger() {
+    for (const key in this) {
+      if (typeof this[key] === "function") {
+        console.log(`${key}: метод`);
+      } else {
+        console.log(`${key}:`, this[key]);
+      }
+    }
+
+    console.log(`Название проекта: ${appData.title}`);
+    console.log(appData.discount(appData.getFullPrice()));
+    console.log(`Стоимость верстки экранов ${appData.screenPrice} рублей`);
+    console.log(
+      `Сумма всех дополнительных услуг: ${appData.getAllServicePrices()} рублей`,
+    );
+    console.log(
+      `Стоимость верстки и всех дополнительных услуг: ${appData.getFullPrice()} рублей`,
+    );
+    console.log(
+      `Стоимость с учетом отката: ${appData.getServicePercentPrices()} рублей`,
+    );
+    console.log(appData.lowCase);
+  },
+
+  start() {
+    appData.asking();
+    appData.title = appData.getTitle();
+    appData.allServicePrices = appData.getAllServicePrices();
+    appData.fullPrice = appData.getFullPrice();
+    appData.servicePercentPrices = appData.getServicePercentPrices();
+    appData.lowCase = appData.screens.toLowerCase().split(", ");
+    appData.logger();
+  },
 };
 
-function getTitle(title) {
-  let clearTitle = title.trim();
-  if (clearTitle === "") {
-    return "";
-  }
-  return clearTitle[0].toUpperCase() + clearTitle.slice(1).toLowerCase();
-}
-
-title = getTitle(title);
-
-function getServicePercentPrices() {
-  return fullPrice - percent;
-}
-
-let servicePercentPrices = getServicePercentPrices();
-
-const counterPotatoes = function (potatoes) {
-  for (let i = 0; i < potatoes.length; i++) {
-    console.log(`Картошка №${i + 1} почищена!`);
-  }
-};
-
-counterPotatoes([1, 2, 3, 4, 5]);
-
-console.log(`Название проекта: ${title}`);
-console.log(discount(fullPrice));
-console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
-console.log(typeof allServicePrices + " -Это тип переменной allServicePrices");
-console.log(`Сумма всех дополнительных услуг: ${allServicePrices} рублей`);
-console.log(
-  `Стоимость верстки и всех дополнительных услуг: ${fullPrice} рублей`,
-);
-console.log(`Стоимость с учетом отката: ${Math.ceil(servicePercentPrices)}`);
-
-console.log(lowCase);
+appData.start();
